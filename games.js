29e580a3e28ac2a6974f75c346d36f222b29ece8
John Harold Dela cruz <jhdlc12@gmail.com>
2022-11-24 11:00:33 +0800

    added home and shop page layout

---
+++ games.js
@@ -0,0 +1,16 @@
+const games = [
+  {
+    name: "Super Mario 64",
+    porduct_console: "Nintendo 64",
+    product_type: "game",
+    price: "$39.99",
+    image: "./images/super-mario-64.jpg",
+  },
+  {
+    name: "Mario Kart 64",
+    porduct_console: "Nintendo 64",
+    product_type: "game",
+    price: "$44.99",
+    image: "./images/mario-kart-64.jfif",
+  },
+];
