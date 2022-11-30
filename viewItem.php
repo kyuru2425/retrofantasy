f5af7901db253fd4c5218bb87746afec462726d2
John Harold Dela cruz <jhdlc12@gmail.com>
2022-11-30 15:25:20 +0800

    added working cart

--- viewItem.php
+++ viewItem.php
@@ -27,7 +27,7 @@
     <h1>Item Details</h1>
     <div class='viewItem-innercontainer'>
         <div class='viewItem-innercontainer-left'>
-            <img src="./ProductImages/Nintendo/Game_Images/3DS/mariokart7.jpg" style='width:100%'>
+            <img src=<?php echo $row4['image'] ?> style='width:100%'>
         </div>
         <div class='viewItem-innercontainer-right' style="max-width: 50%;">
            
@@ -37,14 +37,18 @@
             <h5 class='ms-5'>Console: <?php echo $row4['console_name'] ?></h5>
             <h5 class='ms-5'>Type: <?php echo $row4['type_name'] ?></h4>
             
-            <h3>Stocks available:<span id='stock'>5</span></h3>
-            <h3>Quantity:<button id='qtyUp' type="button"><i class="bi bi-caret-up-fill"></i></button><div id='qty' style="display:inline-block;width:50px;text-align:center">1</div> <button id="qtyDown" type="button"><i class="bi bi-caret-down-fill"></i></button></h3>
-           
-            <form action="addtocart.php" method='post'>
+            <h3>item limit per purchase:<span id='stock'>5</span></h3>
+
+            
+            <form   action="addtocart.php" method='post'>
+            <h3>Quantity:
+            <button id='qtyUp' type="button"><i class="bi bi-caret-up-fill"></i></button> 
+            <input style="display:inline-block;width:50px; border:none;text-align:center;background:transparent" type="text" name="qty" id="qty" value="1" >   
+            <button id="qtyDown" type="button"><i class="bi bi-caret-down-fill"></i></button></h3>
                 <input type="hidden" name='product_id' value='<?php echo $row4['product_id'] ?>'>
                 <button class='btn btn-primary' type='submit' name='addtocart'>Add to Cart</button>
             </form>
-           
+            
         </div>    
     
     </div>
@@ -70,14 +74,14 @@
         qtyUp.addEventListener('click',()=>{
            if( varQty < maxPurchase){
             varQty += 1;
-                qty.innerHTML = varQty;
+                qty.value = varQty;
            }
                
         });
         qtyDown.addEventListener('click',()=>{
             if( varQty >1){
                 varQty -= 1;
-            qty.innerHTML = varQty;
+            qty.value = varQty;
            }
             
         })
